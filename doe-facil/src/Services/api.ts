5d4786200a582fc2652d015fc07583c065c06d51
PatrickEN-dev <patrickandreia2505@gmail.com>
2023-03-08 10:45:31 -0300

    fix requests conflits

--- doe-facil/src/Services/api.ts
+++ doe-facil/src/Services/api.ts
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export const API = axios.create({
   baseURL: "http://localhost:3001",
-  timeout: 10000,
+  timeout: 5000,
 });
--- doe-facil/src/contexts/DashboardContext/UserContext.tsx
+++ doe-facil/src/contexts/DashboardContext/UserContext.tsx
@@ -1,42 +1,60 @@
 import { createContext } from "react";
-import { API } from "../../Services/API";
-import { IUserContextProps, IChildrenProps } from "./inteface";
+import { IUserContextProps, IChildrenProps  } from "./inteface";
 
-export const IUserContext = createContext<IUserContextProps | null>(null);
+export const IUserContext = createContext<IUserContextProps | null>(null)
 
-export const UserProvider = ({ children }: IChildrenProps) => {
-  const token = localStorage.getItem("@userToken");
+export const UserProvider = ({children}: IChildrenProps) => {
+
   const deleteUser = async () => {
+    const token = localStorage.getItem("@userToken")
+
     try {
-      const response = await API.delete("/users/4", {
+      const response = await api.delete("/users/4", {
         headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
-      console.log(response);
+          Authorization: `Bearer ${token}`
+        }
+      })
+      console.log(response)
     } catch (error) {
-      console.log(error);
+      console.log(error)
     }
-  };
+  }
 
   const updateUser = async () => {
-    const token = localStorage.getItem("@userToken");
+    const token = localStorage.getItem("@userToken")
 
     try {
-      const response = await API.patch("/users/4", {
+      const response = await api.patch("/users/4", {
         headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
-      console.log(response);
+          Authorization: `Bearer ${token}`
+        }
+      })
+      console.log(response)
     } catch (error) {
-      console.log(error);
+      console.log(error)
     }
-  };
+  }
 
-  return (
-    <IUserContext.Provider value={{ deleteUser, updateUser }}>
+  // useEffect(() => {
+  //   const getUser = () => {
+  //     try {
+  //       setLoading(true)
+
+  //       const response = await api.get("/users/2/")
+
+
+  //       //consertar quando testar requisição, ainda não sei onde está a resposta da api
+  //       // setUsers(response.data)
+  //     } catch (error) {
+  //       console.log(error)
+  //     } 
+  //   }
+  //   getUser()
+  // }, [])
+
+  return(
+    <IUserContext.Provider value={{deleteUser, updateUser}}>
       {children}
     </IUserContext.Provider>
-  );
-};
+  )
+}
