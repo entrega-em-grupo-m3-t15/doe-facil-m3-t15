d323d358bc0da984701fafb3980dc6334b5042d2
andrzcavalcante <andrzcavalcante@gmail.com>
2023-03-08 09:53:58 -0300

    feat: LoginPage/Forms

--- doe-facil/src/App.tsx
+++ doe-facil/src/App.tsx
@@ -1,7 +1,9 @@
 import { Router } from "./routes/routes";
+import { GlobalStyles } from "./styles/components/globalStyle";
 
 export const App = () => (
   <>
+    <GlobalStyles/>
     <Router />
   </>
 );
