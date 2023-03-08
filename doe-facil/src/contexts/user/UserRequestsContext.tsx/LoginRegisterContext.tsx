8edd5a3141ada935fe3f5a7e264c42c915e3ef25
Uriel Hapuque <urihapuque11@gmail.com>
2023-03-08 19:19:11 -0300

    fix: fix user info and donations contexts

--- doe-facil/src/contexts/user/UserRequestsContext.tsx/LoginRegisterContext.tsx
+++ doe-facil/src/contexts/user/UserRequestsContext.tsx/LoginRegisterContext.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import { createContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
-import { API } from "../../../Services/Api";
+import { API } from "../../../Services/api";
 
 import {
   iAxiosError,
