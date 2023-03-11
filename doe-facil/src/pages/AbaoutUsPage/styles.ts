91cfad79c6079f2d073fd3dc9f69bcda674d587b
Uriel Hapuque <urihapuque11@gmail.com>
2023-03-11 15:59:31 -0300

    style: add home page styles

--- doe-facil/src/pages/AbaoutUsPage/styles.ts
+++ doe-facil/src/pages/AbaoutUsPage/styles.ts
@@ -1,25 +1,36 @@
 import styled from "styled-components";
 
 export const StyledAboutUs = styled.div`
+  header{
+    padding-bottom: 20px;
+  }
+
+
+  @media(max-width: 425px){
+    nav{
+    width: 100px;
+  }
+  }
+  
 
   h2{
     font-weight:800;
-    font-size: 36px;
+    font-size: 2.25rem;
     color:var(--Color-primary);
-    margin-bottom: 30px;
-    margin-top: 41px;
+    margin-bottom: 1.875rem;
+    margin-top: 2.563rem;
     text-align: center;
   }
 
   main{
 
-    margin-inline: 20px;
+    margin-inline: 1.25rem;
 
     p{
       font-weight: 400;
-      font-size: 16px;
+      font-size: 1rem;
       color: var(--Color-primary);
-      line-height: 24px;
+      line-height: 1.5rem;
     }
   }
 
@@ -28,19 +39,19 @@ export const StyledAboutUs = styled.div`
     flex-direction: column;
     align-items: center;
 
-    margin-top: 40px;
-    margin-bottom: 40px;
+    margin-top: 2.5rem;
+    margin-bottom: 2.5rem;
 
     h3{
       font-weight: 800;
-      font-size: 28px;
+      font-size: 1.75rem;
       color: var(--Color-primary);
-      margin-bottom: 20px;
+      margin-bottom: 1.25rem;
     }
     section{
       display: grid;
       grid-template-columns: repeat(2, 1fr);
-      gap: 20px;
+      gap: 1.25rem;
 
 
 
@@ -48,18 +59,18 @@ export const StyledAboutUs = styled.div`
         display: flex;
         flex-direction: column;
         align-items: center;
-        gap: 15px;
-        margin-top: 27px;
+        gap: 0.938rem;
+        margin-top: 1.688rem;
 
         img{
-          width: 78px;
-          border-radius: 152px;
+          width: 4.875rem;
+          border-radius: 9.5rem;
         }
 
         h4{
           color: var(--Color-primary);
           font-weight: 800;
-          font-size: 14px;
+          font-size: 0.875rem;
         }
       }
     }
@@ -67,11 +78,11 @@ export const StyledAboutUs = styled.div`
     @media(min-width: 800px){
       section{
         grid-template-columns: repeat(4, 1fr);
-        gap: 20px;
+        gap: 1.25rem;
         
         div{
           img{
-           width: 100px;
+           width: 6.25rem;
           }
         }
       }  
@@ -80,11 +91,11 @@ export const StyledAboutUs = styled.div`
     @media(min-width: 1300px){
       section{
         grid-template-columns: repeat(4, 1fr);
-        gap: 20px;
+        gap: 1.25rem;
         
         div{
           img{
-           width: 173px;
+           width: 10.813rem;
           }
         }
       }  
@@ -103,7 +114,7 @@ export const StyledAboutUs = styled.div`
         display: flex;
         width: 40%;
         height: 100vh;
-        margin-top: 20px;
+        margin-top: 1.25rem;
       }
     }
   }
